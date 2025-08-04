f140bdb1838bef3b12b4bafed223644be4144a09
Yudisssss <xaulintobi@gmail.com>
2025-08-04 12:01:11 -0300

    chore(src): change Main.java

--- FlowApplication/src/Main.java
+++ FlowApplication/src/Main.java
@@ -1,10 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         User p1 = new User();
-
-        p1.name = "Flavio";
-        p1.age = 17;
-
-        p1.AgeVerificattion(p1.age);
+        
     }
 }
